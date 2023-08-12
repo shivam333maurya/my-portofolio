f2b39f070cd02e6a195b6b01060e294c9610093e
Shivam Maurya <flageg333s@gmail.com>
2023-08-12 12:27:05 +0530

    style update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <main className="p-3">
       <div className="h-full flex min-h-[calc(100vh-32px)] max-w-[1111px] m-auto bg-gray-200">
         <div className="text-black flex ">
-          <div className="h-auto bg-green-100 w-full">
-            <div className="h-44 flex bg-green-200 justify-center text-black  flex-col">
+          <div className="h-auto bg-green-100 w-full shadow-[-10px_-10px_40px_5px_lightblue_inset]">
+            <div className="h-44 flex bg-green-200 justify-center text-black shadow-[-10px_82px_100px_5px_#897d7d9c_inset] flex-col">
               <h1 className="text-center text-5xl font-bold mt-0 pt-4">
                 {MY_DATA.profile.name}
               </h1>
